fcb557d735734649fb211c6f29288e8741941a96
鹿野壮 <takeshi.kano@ics-web.jp>
2017-09-22 20:53:55 +0900

    すごくLike: クラス化

--- superlikes/src/js/script.js
+++ superlikes/src/js/script.js
@@ -1,30 +1,59 @@
-async function main() {
-  const response = await fetch("./svg/likeButton.svg");
-  const svgCode = await response.text();
-  const likeButtonCode = svgCode.replace("<svg", `<svg class="likeButton"`);
+class SuperLikes {
+  constructor() {
+    this.init();
+    this.likeList = [];
+  }
+  async init() {
+    // SVGコードのフェッチ
+    const svgCode = await this.fetchSource();
+    // LikeButton用ソースコードに変換
+    const likeButtonCode = svgCode.replace("<svg", `<svg class="likeButton"`);
 
-  const container = document.getElementById("container");
-
-  const likeContainerMaster = document.createElement("div");
-  likeContainerMaster.classList.add("likeContainer");
-  likeContainerMaster.innerHTML = likeButtonCode;
-
-  const likeList = [];
-
-  const likeNum = 40;
-
-  for (let i = 0; i < likeNum; i++) {
-    const likeContainer = likeContainerMaster.cloneNode(true);
-    container.appendChild(likeContainer);
-    likeList.push(likeContainer);
+    // Likeボタンをレイアウト
+    this.addLikeButtons(likeButtonCode);
   }
 
-  // 全世界同時バージョン
-  addEventListener("click", () => {
-    likeList.forEach((likeContainer) => {
+  async fetchSource() {
+    const response = await fetch("./svg/likeButton.svg");
+    const svgCode = await response.text();
+
+    return svgCode;
+  }
+
+  addLikeButtons(likeButtonCode) {
+
+    const container = document.getElementById("container");
+
+    const likeContainerMaster = document.createElement("div");
+    likeContainerMaster.classList.add("likeContainer");
+    likeContainerMaster.innerHTML = likeButtonCode;
+
+    const likeNum = 40;
+
+    for (let i = 0; i < likeNum; i++) {
+      const likeContainer = likeContainerMaster.cloneNode(true);
+      container.appendChild(likeContainer);
+      this.likeList.push(likeContainer);
+    }
+
+    this.addEvents();
+  }
+
+  addEvents() {
+    // イベント設定
+    //window.addEventListener("click", () => this.onClick());
+    window.addEventListener("touchstart", () => this.onClick());
+  }
+
+  onClick() {
+    if (!this.likeList) {
+      return;
+    }
+
+    this.likeList.forEach((likeContainer) => {
       likeContainer.classList.toggle("clicked");
-    })
-  });
+    });
+  }
 }
 
-main();
+new SuperLikes();
