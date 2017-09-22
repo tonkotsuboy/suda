0a101b15dde56a9d56402558baca8623d49c3b22
tonkotsuboy <t.kano.624@gmail.com>
2017-09-23 01:00:14 +0900

    すごくLikeです：Suda ver

---
+++ superlikes/src/js/suda.js
@@ -0,0 +1,59 @@
+class SuperLikes {
+  constructor() {
+    this.init();
+    this.likeList = [];
+  }
+  async init() {
+    // SVGコードのフェッチ
+    const svgCode = await this.fetchSource();
+    // LikeButton用ソースコードに変換
+    const likeButtonCode = svgCode.replace("<svg", `<svg class="likeButton"`);
+
+    // Likeボタンをレイアウト
+    this.addLikeButtons(likeButtonCode);
+  }
+
+  async fetchSource() {
+    const response = await fetch("./svg/sudaButton.svg");
+    const svgCode = await response.text();
+
+    return svgCode;
+  }
+
+  addLikeButtons(likeButtonCode) {
+
+    const container = document.getElementById("container");
+
+    const likeContainerMaster = document.createElement("div");
+    likeContainerMaster.classList.add("likeContainer");
+    likeContainerMaster.innerHTML = likeButtonCode;
+
+    const likeNum = 40;
+
+    for (let i = 0; i < likeNum; i++) {
+      const likeContainer = likeContainerMaster.cloneNode(true);
+      container.appendChild(likeContainer);
+      this.likeList.push(likeContainer);
+    }
+
+    this.addEvents();
+  }
+
+  addEvents() {
+    // イベント設定
+    window.addEventListener("click", () => this.onClick());
+    window.addEventListener("touchstart", () => this.onClick());
+  }
+
+  onClick() {
+    if (!this.likeList) {
+      return;
+    }
+
+    this.likeList.forEach((likeContainer) => {
+      likeContainer.classList.toggle("clicked");
+    });
+  }
+}
+
+new SuperLikes();
