73ba5064a971ba0f60d523ce166754500e67286d
ics-kano <takeshi.kano@ics-web.jp>
2018-03-26 14:59:20 +0900

    殺伐としたTLに突然のものすごいコミットが！

--- superlikes/src/js/suda2.js
+++ superlikes/src/js/suda2.js
@@ -1,96 +1,88 @@
-class SuperLikes {
-  constructor() {
-    this.likeList = [];
-    this.timerList = [];
-    this.isOn = false;
-
-    this.init();
-  }
-
-  init() {
-    Promise.all([
-      this.fetchSource(),
-      this.loadSe()
-    ]);
-  }
-
-  async fetchSource() {
-    const response = await fetch("./svg/sudaButton.svg");
-    const svgCode = await response.text();
-    // LikeButton用ソースコードに変換
-    const likeButtonCode = svgCode.replace("<svg", `<svg class="likeButton"`);
-
-    // Likeボタンをレイアウト
-    this.addLikeButtons(likeButtonCode);
-  }
-
-  async loadSe() {
-    return new Promise(resolve => {
-      createjs.Sound.alternateExtensions = ["mp3"];
-      createjs.Sound.registerSound("mp3/people-people-shout-oo1.mp3", "sound");
-      createjs.Sound.on("fileload", () => {
-        resolve();
-      }, this);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  }
-
-  addLikeButtons(likeButtonCode) {
-    const container = document.getElementById("container");
-
-    const likeContainerMaster = document.createElement("div");
-    likeContainerMaster.classList.add("likeContainer");
-    likeContainerMaster.innerHTML = likeButtonCode;
-
-    const likeNum = 24;
-
-    for (let i = 0; i < likeNum; i++) {
-      const likeContainer = likeContainerMaster.cloneNode(true);
-      container.appendChild(likeContainer);
-      this.likeList.push(likeContainer);
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-
-    this.addEvents();
-  }
-
-  addEvents() {
-    // イベント設定
-    window.addEventListener("click", () => this.onClick());
-    window.addEventListener("touchstart", () => this.onClick());
-  }
-
-  onClick() {
-    if (!this.likeList) {
-      return;
+};
+var Main = /** @class */ (function () {
+    function Main() {
+        this.sePath = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/221808/people-people-shout-oo1.mp3";
+        this.seName = "アオッ！";
+        this.brightnessThreshold = {
+            min: 0,
+            max: 250
+        };
+        this.brightness = 0;
+        this.gain = 5;
+        this.init();
     }
-
-    this.timerList.forEach((timer) => {
-      clearTimeout(timer);
-    });
-
-    if (this.isOn) {
-      this.likeList.forEach((likeContainer) => {
-        likeContainer.classList.remove("clicked");
-      });
-    }
-    else {
-      this.timerList = [];
-
-      createjs.Sound.play("sound");
-
-      this.likeList.forEach((likeContainer, i) => {
-        const timer = setTimeout(() => {
-          likeContainer.classList.add("clicked");
-          if (i % 2 === 0) {
-            createjs.Sound.play("sound");
-          }
-        }, i * 100);
-
-        this.timerList.push(timer);
-      });
-    }
-
-    this.isOn = !this.isOn;
-  }
-}
-
-new SuperLikes();
+    Main.prototype.init = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.loadSe()];
+                    case 1:
+                        _a.sent();
+                        this.suda = document.querySelector("#suda");
+                        window.addEventListener("click", function () { return _this.click(); });
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Main.prototype.loadSe = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _this = this;
+            return __generator(this, function (_a) {
+                return [2 /*return*/, new Promise(function (resolve) {
+                        createjs.Sound.registerSound(_this.sePath, _this.seName);
+                        createjs.Sound.on("fileload", function () {
+                            resolve();
+                        }, _this);
+                    })];
+            });
+        });
+    };
+    Main.prototype.click = function () {
+        createjs.Sound.play(this.seName);
+        this.brightness += this.gain;
+        if (this.brightness <= this.brightnessThreshold.min
+            || this.brightness >= this.brightnessThreshold.max) {
+            this.gain = -this.gain;
+            return;
+        }
+        this.suda.style.filter = "brightness(" + this.brightness + "%)";
+    };
+    return Main;
+}());
+addEventListener("DOMContentLoaded", function () { return new Main(); });
