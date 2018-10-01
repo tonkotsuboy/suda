d1ad4fb9222cc07f255922aa883737d547328d9e
tonkotsuboy <t.kano.624@gmail.com>
2018-10-01 20:26:10 +0900

    ダブルス！

--- celebration/app.js
+++ celebration/app.js
@@ -1,1 +1,27 @@
 import "./style.scss";
+
+document.documentElement.addEventListener(
+  "touchstart",
+  e => {
+    if (e.touches.length >= 2) {
+      e.preventDefault();
+    } else {
+    }
+  },
+  { passive: false }
+);
+
+let lastTouchEndTime = 0;
+document.documentElement.addEventListener(
+  "touchend",
+  e => {
+    const now = window.performance.now();
+    if (now - lastTouchEndTime <= 300) {
+      e.preventDefault();
+    }
+    lastTouchEndTime = now;
+  },
+  { passive: false }
+);
+
+
