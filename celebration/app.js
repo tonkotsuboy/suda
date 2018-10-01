4ede2dc2138e3ae53233b28ac73833468385c026
tonkotsuboy <t.kano.624@gmail.com>
2018-10-01 20:26:10 +0900

    全然ピンチじゃないじゃない

--- celebration/app.js
+++ celebration/app.js
@@ -1,1 +1,12 @@
 import "./style.scss";
+
+document.documentElement.addEventListener(
+  "touchstart",
+  e => {
+    if (e.touches.length >= 2) {
+      e.preventDefault();
+    } else {
+    }
+  },
+  { passive: false }
+);
